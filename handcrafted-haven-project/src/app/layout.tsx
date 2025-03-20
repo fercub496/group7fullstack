9eb9bbdb77050a2ed8ad81d0144d813d3ef8e9d2
AndreyBlanco <andygblanco@gmail.com>
2025-03-19 22:14:21 -0600

    fonts and colors

--- handcrafted-haven-project/src/app/layout.tsx
+++ handcrafted-haven-project/src/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Montserrat } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const geistSans = Montserrat({
+  style: ["normal"],
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const geistMono = Montserrat({
+  weight: ["400"],
+  style: ["italic"],
   subsets: ["latin"],
 });
 
@@ -24,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistMono.className} ${geistSans.className}`}>
         {children}
       </body>
     </html>
